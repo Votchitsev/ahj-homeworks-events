c79b5fb1c1cdccab712e3d2f50b337bb998cfb01
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-08-12 19:02:37 +0200

    yarn init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+      port: 3000,
+      open: true,
+  },
+};
